92e9c54b74533c286d558605e976015ce70111f4
nesscollege2016 <nesscollege2016@gmail.com>
2016-05-16 08:34:31 +0300

    fixed a problem

--- app/src/main/java/ness/tomerbu/edu/toolbarsdemo/MainActivity.java
+++ app/src/main/java/ness/tomerbu/edu/toolbarsdemo/MainActivity.java
@@ -21,12 +21,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(
-            int requestCode,
-            String[] permissions,
-             int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_CALL){
-            if (permissions[0] == Manifest.permission.CALL_PHONE){
+            if (permissions[0].equals(Manifest.permission.CALL_PHONE)){
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
                     call(null);
                 }
